c30193e0a4f0c2de14e909a533aa1f79e240fb88
MOSELLE Marie-Luc <72310983+MOBISARE@users.noreply.github.com>
2022-03-12 21:36:01 +0100

    A ring
    A golden ring is rotating now in the middle of the screen

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three'
+import { AmbientLight } from 'three';
 
 const scene = new THREE.Scene();
 
@@ -8,28 +9,33 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
-
 });
 
 renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(windows.innerWidth, window.innerHeight);
-document.canvas.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild( renderer.domElement );
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
+const material = new THREE.MeshStandardMaterial({color: 0xFFD700});
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus)
 
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20, 20, 20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight)
+
 function animate() {
   requestAnimationFrame(animate);
-  torus.rotation.x += 0.1;
+  torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
-  torus.rotation.z += 0.1;
+  torus.rotation.z += 0.01;
   renderer.render(scene, camera);
 }
 animate()
